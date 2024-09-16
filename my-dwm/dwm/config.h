879f047734189078c230660120462410c56f898c
gentootoaster <lobachev.arseniy@gmail.com>
2024-09-16 20:16:24 +0500

    Add files via upload

--- my-dwm/dwm/config.h
+++ my-dwm/dwm/config.h
@@ -2,14 +2,14 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "JetBrains Mono:style=Bold:size=14" };
-static const char dmenufont[]       = "JetBrains Mono:style=Bold:size=14";
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const int vertpad            = 15;       /* vertical padding of bar */
+static const int sidepad            = 500;       /* horizontal padding of bar */
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:style=Bold:size=14" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:style=Bold:size=14";
 static const char col_gray1[]       = "#24273a";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#cad3f5";
